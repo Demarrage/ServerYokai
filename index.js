42991cebf359ad0a41aa51f7aba88636b8f90831
GUILHERME VICTOR BOTA SALOMAO <guilherme.vbsalomao@senacsp.edu.br>
2021-05-18 09:52:52 -0300

    cadastro finalizado

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const saltRound = 10;
 
 const app = express();
 app.use(bodyParser.json());
@@ -27,12 +29,20 @@ cx.connect((error, dados) => {
 });
 // rotas para o usuario
 app.post("/usuario/cadastro", cors(configCors), (req, res) => {
-  cx.query("insert into tbusuario set ?", [req.body], (error, result) => {
-    if (error) {
-      res.status(400).send({ output: `nao cadastrou -> ${error}` });
-      return;
-    }
-    res.status(201).send({ output: result });
+  const sh = req.body.senha;
+  const us = req.body.nomeusuario;
+  bcrypt.hash(sh, saltRound).then((senha) => {
+    cx.query(
+      "insert into tbusuario set nomeusuario=?,senha=?",
+      [us, senha],
+      (error, result) => {
+        if (error) {
+          res.status(400).send({ output: `nao cadastrou -> ${error}` });
+          return;
+        }
+        res.status(201).send({ output: result });
+      }
+    );
   });
 });
 app.get("/usuario/listar", cors(configCors), (req, res) => {
@@ -66,24 +76,76 @@ app.post("/usuario/login", cors(configCors), (req, res) => {
   const cf = req.body.cpf;
   const em = req.body.email;
   const sh = req.body.senha;
+
   cx.query(
     `select u.* from tbusuario u
-    inner join tbcliente cl
-    on u.idusuario = cl.idusuario
-    inner join tbcontato co
-    on cl.idcontato = co.idcontato
-    where (u.nomeusuario=? or cl.cpf=? or co.email=?) and u.senha=?;`,
-    [us, cf, em, sh],
-    (error, result) => {
-      if (error) {
-        res
-          .status(400)
-          .send({ output: `Erro ao tentar efetuar login -> ${error}` });
-        return;
-      }
-      res.status(200).send({ output: result });
+  inner join tbcliente cl
+  on u.idusuario = cl.idusuario
+  inner join tbcontato co
+  on cl.idcontato = co.idcontato
+  where (u.nomeusuario=? or cl.cpf=? or co.email=?)`,
+    [us, cf, em],
+    (e, rs) => {
+      if (e) return res.status(400).send({ output: e });
+
+      bcrypt.compare(sh, rs[0].senha).then((resultado) => {
+        if (resultado) {
+          res.status(200).send({ output: "Logado" });
+        } else {
+          res.status(404).send({ output: "Usuario não localizado" });
+        }
+      });
     }
   );
+
+  // cx.query(
+  //   `select u.* from tbusuario u
+  //   inner join tbcliente cl
+  //   on u.idusuario = cl.idusuario
+  //   inner join tbcontato co
+  //   on cl.idcontato = co.idcontato
+  //   where (u.nomeusuario=? or cl.cpf=? or co.email=?) and u.senha=?;`,
+  //   [us, cf, em, sh],
+  //   (error, result) => {
+  //     if (error) {
+  //       res
+  //         .status(400)
+  //         .send({ output: `Erro ao tentar efetuar login -> ${error}` });
+  //       return;
+  //     }
+  //     res.status(200).send({ output: result });
+  //   }
+  // );
+});
+// rotas para o contato
+app.post("/contato/cadastro", cors(configCors), (req, res) => {
+  cx.query("insert into tbcontato set ?", [req.body], (error, result) => {
+    if (error) {
+      res.status(400).send({ output: `nao cadastrou -> ${error}` });
+      return;
+    }
+    res.status(201).send({ output: result });
+  });
 });
 
+// rotas para o endereco
+app.post("/endereco/cadastro", cors(configCors), (req, res) => {
+  cx.query("insert into tbendereco set ?", [req.body], (error, result) => {
+    if (error) {
+      res.status(400).send({ output: `nao cadastrou -> ${error}` });
+      return;
+    }
+    res.status(201).send({ output: result });
+  });
+});
+// rotas para o cliente
+app.post("/cliente/cadastro", cors(configCors), (req, res) => {
+  cx.query("insert into tbcliente set ?", [req.body], (error, result) => {
+    if (error) {
+      res.status(400).send({ output: `nao cadastrou -> ${error}` });
+      return;
+    }
+    res.status(201).send({ output: result });
+  });
+});
 app.listen(5005, () => console.log("Servidor online na porta 5005"));
